ef6e076c12bedbcab9e6e4f268ef36eece96e529
Aleena <Aleenamalikco@gmail.com>
2025-06-13 23:54:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from openai import OpenAI
 from utils import select_with_other, generate_drawio, generate_strategy_pdf
 from flowkind_conductor import run_full_engagement_engine
 
-
 # Configure the page
 st.set_page_config(page_title="FlowKind", layout="centered")
 
